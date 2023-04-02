3cde7a5c4ad7e0b97dcab370782c1ff742b5eb95
Ēriks Lijurovs <eriks.lijurovs@edu.rtu.lv>
2023-04-03 00:17:49 +0300

    221RDB041 minimāli optimizēts?

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+#221RDB041 Ēriks Lijurovs 16. grupa
 # python3
+import time
 
 class Query:
     def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
+        if query[0].isnumeric():
+            self.type = 'invalid'
+            self.number = 0
+        elif query[1].isnumeric() == False:
+            self.type = 'invalid'
+            self.number = 0
+        elif len(query[1]) > 7:
+            self.type = 'invalid'
+            self.number = 0
+        elif query[0] == 'add' and len(query[2]) > 15:
+            self.type = 'invalid'
+            self.number = 0
+        else:
+            self.type = query[0]
+            self.number = int(query[1])
+
         if self.type == 'add':
             self.name = query[2]
 
@@ -17,27 +33,37 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = [[], [], [], [], [], [], [], [], [], []]
+    
     for cur_query in queries:
         if cur_query.type == 'add':
+            index = int(str(cur_query.number)[0])
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
+            for contact in contacts[index]:
                 if contact.number == cur_query.number:
                     contact.name = cur_query.name
+                    
                     break
             else: # otherwise, just add it
-                contacts.append(cur_query)
+                contacts[index].append(cur_query)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
+            index = int(str(cur_query.number)[0])
+            for j in range(len(contacts[index])):
+                if contacts[index][j].number == cur_query.number:
+                    contacts[index].pop(j)
+                    
                     break
+        elif cur_query.type == 'invalid':
+            response = 'invalid query'
+            result.append(response)
         else:
             response = 'not found'
-            for contact in contacts:
+            index = int(str(cur_query.number)[0])
+            for contact in contacts[index]:
                 if contact.number == cur_query.number:
                     response = contact.name
+                    
                     break
             result.append(response)
     return result
